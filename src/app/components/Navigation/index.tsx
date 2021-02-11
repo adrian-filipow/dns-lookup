32552356569fed2e41474e4fb22e83f92c5b749c
Adrian Filipow <adrianfilipow@outlook.de>
2021-02-11 04:06:52 +0100

    Opt navigation

---
+++ src/app/components/Navigation/index.tsx
@@ -0,0 +1,38 @@
+/**
+ *
+ * Navigation
+ *
+ */
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+/**
+ * Bootstrap imports
+ */
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+interface Props {}
+
+export function Navigation(props: Props) {
+  return (
+    <NavbarWrapper>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>DNS Lookup</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="https://github.com/adrian-filipow/dns-lookup">
+                Github
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </NavbarWrapper>
+  );
+}
+
+const NavbarWrapper = styled.div``;
