515a5b3055131e4c93773d6a944f00103bb53b6d
Adrian Filipow <adrianfilipow@outlook.de>
2021-02-11 04:09:01 +0100

    Edit homepage

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -3,8 +3,22 @@
  * HomePage
  *
  */
+
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+
+import { Navigation } from 'app/components/Navigation';
+import { DnsLookupForm } from 'app/components/DnsLookupForm';
+
+import styled from 'styled-components/macro';
+
+/**
+ * Bootstrap imports
+ */
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { DnsResults } from 'app/components/DnsLookupForm/DnsResults';
 
 interface Props {}
 
@@ -15,7 +29,21 @@ export function HomePage(props: Props) {
         <title>Home Page</title>
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
-      <span>HomePage container</span>
+      <Navigation />
+      <HomePageWrapper>
+        <Container>
+          <Row>
+            <Col>
+              <DnsLookupForm />
+              <DnsResults />
+            </Col>
+          </Row>
+        </Container>
+      </HomePageWrapper>
     </>
   );
 }
+
+const HomePageWrapper = styled.div`
+  padding: ${p => p.theme.sectionSpacing / 2}px;
+`;
